6e2964093d795bb624dd03c62dfea61099409a04
Hweichi Meng <121360116+nurebiya@users.noreply.github.com>
2025-05-17 03:55:33 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const textEl = document.querySelector("#greetings");
 const original = JSON.parse(textEl.getAttribute("data-greeting"));
+const markEl = document.querySelector(".mark");
 
 const first = "Hello";
 const rest = original.filter(text => text !== first);
+
+const UtD = new Set(["ᠰᠠᠢᠨ ᠤᠤ", "ᠰᠠᡳᠶᡡᠨ"]);
+const RtL = new Set(["ܫܠܡܐ", "مرحبا", "שלום", "سلام", "𒁲"]);
 
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -30,6 +34,21 @@ function type(time){
 
     if(progress > delta) {
         let greetings = texts[index];
+
+        if (UtD.has(greetings)) {
+            textEl.style.direction = "ltr";
+            textEl.style.writingMode = "vertical-rl";
+            textEl.style.textOrientation = "mixed";
+        } else if (RtL.has(greetings)) {
+            textEl.style.direction = "rtl";
+            textEl.style.writingMode = "horizontal-tb";
+            textEl.style.textOrientation = "mixed";
+        } else {
+            textEl.style.direction = "ltr";
+            textEl.style.writingMode = "horizontal-tb";
+            textEl.style.textOrientation = "mixed";
+        }
+
         if(!isDeleting) {
             textEl.innerHTML = greetings.slice(0, ++charIndex);
             delta = 500 - Math.random() * 400;
@@ -49,6 +68,7 @@ function type(time){
             isDeleting = false;
             start = time + 200;
             index = ++index % texts.length
+    
         }
     }
 }
